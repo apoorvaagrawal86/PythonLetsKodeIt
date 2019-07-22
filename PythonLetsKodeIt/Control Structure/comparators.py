b4ce04ac30a06ae610efecfa82662093ec6af4fc
Apoorva Agrawal <apoorva.agrawal.86@gmail.com>
2019-07-22 17:25:38 +0200

    Added new package control structure

---
+++ PythonLetsKodeIt/Control Structure/comparators.py
@@ -0,0 +1,22 @@
+"""
+== value equality
+!= Not equal to
+< Less than
+> Greater than
+<= Less than equal to
+>= Greater than equal to
+"""
+
+bool_one = 10 == 10
+not_equal = 10 != 10
+less_than = 10 < 10
+greater_than = 10 > 10
+lt_eq = 10 <= 10
+gt_eq = 11 >= 10
+print(bool_one)
+print(not_equal)
+print(less_than)
+print(greater_than)
+print(lt_eq)
+print(gt_eq)
+
