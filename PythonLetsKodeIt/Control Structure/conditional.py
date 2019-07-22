dbc0645449a0df247f33023cde2e6115e25836ca
Apoorva Agrawal <apoorva.agrawal.86@gmail.com>
2019-07-22 23:30:55 +0200

    adding files for conditions and loops except for loop

---
+++ PythonLetsKodeIt/Control Structure/conditional.py
@@ -0,0 +1,16 @@
+"""
+conditional logic
+"""
+
+if 100 > 10:
+    print("100 is greater than 10")
+
+value = "Yellow"
+if value == "green":
+    print("Go")
+elif value == "Yellow":
+    print("Prepare to stop")
+else:
+    print("Stop")
+
+print("It will always print")
